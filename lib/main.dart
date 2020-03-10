4169351965e61541ce371815b55b221789e3f83f
huangxiaoshen <huangxiaoshen@castelbds.com>
2020-03-10 17:59:48 +0800

    修改样式

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:ioschedflutter/scheduleItem.dart';
 
-void main() => runApp(MyApp());
+void main() {
+//  debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
-  final List<Tab> tabs = <Tab>[Tab(text: "Oct 23"), Tab(text: "Oct 24",)];
+  final List<Tab> tabs = <Tab>[
+    Tab(text: "Oct 23"),
+    Tab(
+      text: "Oct 24",
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +22,22 @@ class MyApp extends StatelessWidget {
         title: 'Schedule',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primaryColor: Color(0xFF303030),
+          primaryColor: Color(0xFF202020),
+          primarySwatch: Colors.green
         ),
         home: DefaultTabController(
           length: tabs.length,
           child: Scaffold(
             appBar: AppBar(
               centerTitle: true,
-              leading: Icon(Icons.account_circle, size: 32,color: Colors.green),
-              actions: <Widget>[Padding(padding: EdgeInsets.only(right:20), child: Icon(Icons.search),)] ,
+              leading:
+                  Icon(Icons.account_circle, size: 32, color: Colors.green),
+              actions: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Icon(Icons.search),
+                )
+              ],
               title: Text('Schedule'),
               bottom: TabBar(
                 tabs: tabs,
@@ -29,11 +45,10 @@ class MyApp extends StatelessWidget {
             ),
             body: TabBarView(
               children: tabs.map((Tab tab) {
-                return ScheduleItem();
+                return Container(color: Colors.black, child: ScheduleItem());
               }).toList(),
             ),
           ),
-        )
-    );
+        ));
   }
 }
--- lib/scheduleItem.dart
+++ lib/scheduleItem.dart
@@ -3,23 +3,68 @@ import 'package:flutter/material.dart';
 class ScheduleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Column(
-          children: <Widget>[Text("10"), Text("上午")],
-        ),
-        Expanded(
-          flex: 1,
-          child: Column(
+    return DefaultTextStyle(
+      style: TextStyle(color: Colors.white70),
+      child: Column(
+        children: <Widget>[
+          Row(
             children: <Widget>[
-              Text("Keynote"),
-              Text("Main stage"),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "8:30",
+                      style: TextStyle(color: Colors.green, fontSize: 26),
+                    ),
+                    Text(
+                      "上午",
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Keynote",
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.videocam, color: Colors.white60),
+                        Text("1 hours / Main Stage")
+                      ],
+                    ),
+                    Wrap(
+                      spacing: 4,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 6,
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        Text("Keynote")
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Icon(
+                    Icons.star_border,
+                    color: Colors.white70,
+                    size: 26,
+                  ))
             ],
           ),
-        ),
-        Icon(Icons.star_border)
-      ],
+        ],
+      ),
     );
   }
 }
